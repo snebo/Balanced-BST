d27bf4a3ed6ddeadf42b0f52c26f7ee563fa79e3
Stefano <snebo54@hotmail.com>
2024-01-05 00:41:55 +0100

    removed redudant code

--- lib/balanced_bst.rb
+++ lib/balanced_bst.rb
@@ -62,7 +62,6 @@ class Tree
 
     h_left = height(node.left)
     h_right = height(node.right)
-    puts "left #{h_left}, right#{h_right}"
     (h_left - h_right) <= 1
   end
 
@@ -201,29 +200,3 @@ class Tree
     node
   end
 end
-
-arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7]
-tree = Tree.new(arr)
-tree.pretty_print
-tree.insert(6)
-tree.insert(20)
-tree.pretty_print
-puts "Left most: #{tree.find_min}"
-puts "contains '8' = #{tree.contain?(8)}"
-tree.delete(4)
-tree.delete(9)
-tree.pretty_print
-puts "'8' is : #{tree.find(8)}"
-p tree.in_order_travel
-p tree.post_order_travel
-p tree.level_order
-p tree.pre_order_travel
-puts "The node's height is:#{tree.height}"
-puts "Node depth: #{tree.depth}"
-tree.delete(6)
-tree.delete(8)
-tree.delete(23)
-tree.pretty_print
-puts "Is the node balanced: #{tree.balanced?}"
-tree.rebalance
-tree.pretty_print
